2bd7295a977c00a49ce0e420e24f6aa8096fc506
Jock MacDonald <1501651@abertay.ac.uk>
2019-04-23 12:33:43 +0100

    added barnes hut accuracy test

--- N_Body_Simulation/N_Body_Simulation/SortVector.cpp
+++ N_Body_Simulation/N_Body_Simulation/SortVector.cpp
@@ -20,3 +20,22 @@ std::vector<int> Sort(std::vector<int> list) {
 
 	return list;
 }
+
+
+std::vector<float> Sort(std::vector<float> list) {
+
+	for (int i = 0; i < list.size(); i++) {
+
+		for (int j = i; j < list.size(); j++) {
+
+			if (list.at(j) < list.at(i)) {
+
+				float temp = list.at(i);
+				list.at(i) = list.at(j);
+				list.at(j) = temp;
+			}
+		}
+	}
+
+	return list;
+}
